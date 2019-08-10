1736bad2963437e28e9c2a00d6631eed05d9b08e
SAKETH REDDY <sakethreddy997@gmail.com>
2019-08-10 16:47:21 +0530

    re generated jar file with updates

--- saketh/src/main/java/rubycon/saketh/Coupon.java
+++ saketh/src/main/java/rubycon/saketh/Coupon.java
@@ -3,20 +3,9 @@ package rubycon.saketh;
 import java.util.Random;
 
 public class Coupon {
-    //public static String givenUsingApache_whenGeneratingRandomAlphanumericString_thenCorrect() {
-    // String generatedString = RandomStringUtils.randomAlphanumeric(7);
 
-    //return (generatedString);
-    //}
 
-    //public String givenUsingPlainJava_whenGeneratingRandomStringUnbounded_thenCorrect() {
-    //final byte[] array = new byte[7]; // length is bounded by 7
-    //new Random().nextBytes(array);
-    //final String generatedString = new String(array, Charset.forName("ASCII"));
 
-    //System.out.println(generatedString);
-
-    //return generatedString;
     public String givenUsingPlainJava_whenGeneratingRandomStringBounded_thenCorrect() {
         final int leftLimit = 97; // letter 'a'
         final int rightLimit = 122; // letter 'z'
